affe16f101ad4d537a098957527bbaae13f99081
xair2005 <xair2005@gmail.com>
2022-09-06 23:37:00 +0300

    1.19

--- Lesson_1/CyclesTheme.java
+++ Lesson_1/CyclesTheme.java
@@ -5,15 +5,15 @@ public class CyclesTheme {
         int sumEven = 0;
         int sumOdd = 0;
         do {
-                if (counter % 2 == 0 ) {
-                    sumEven++;
-                } else {
-                    sumOdd++;
-                }
+            if (counter % 2 == 0) {
+                sumEven++;
+            } else {
+                sumOdd++;
+            }
             counter++;
         } while (counter <= 21);
         System.out.println("\nВ промежутке [-10, 21] сумма четных чисел = " + sumEven + 
-                ", а нечетных = " + sumOdd);
+            ", а нечетных = " + sumOdd);
 
         System.out.println("\n 2. Вывод чисел в интервале (min и max) в порядке убывания\n");
         int numb1 = -1;
@@ -34,8 +34,7 @@ public class CyclesTheme {
         if (numb3 > max){
             max = numb3;
         }
-        max--;
-        for (int i = max; i > min; i--) {
+        for (int i = max--; i > min; i--) {
             System.out.print(i + "; ");
         }
 
@@ -48,17 +47,17 @@ public class CyclesTheme {
             digit = numb1 % 10;
             sumDigits += digit;
             System.out.print(digit);
-            numb1 = numb1 / 10;
+            numb1 /= 10;
         }
         System.out.print("\nСумма его цифр: " + sumDigits);
 
         System.out.println("\n\n 4. Вывод чисел на консоль в несколько строк \n");
         
-        for (int j = 1; j < 24; j+=2) {
+        for (int j = 1; j < 24; j += 2) {
             System.out.printf("%4d", j);
             for (int i = 2; i <= 5; i++) {
-                j+=2;
-                if (j < 24 ) {
+                j += 2;
+                if (j < 24) {
                     System.out.printf("%4d", j);
                     if (i % 5 == 0) {
                         System.out.println();
@@ -72,41 +71,41 @@ public class CyclesTheme {
         System.out.println("\n\n 5. Проверка количества единиц на четность");
 
         int srcNumb = 3141591;
-        int j = 0;
+        int countUnit = 0;
         System.out.print("\nЧисло " + srcNumb + " содержит ");
         while (srcNumb > 0) {
-            if (srcNumb % 10 == 1 ) {
-                j++;
+            if (srcNumb % 10 == 1) {
+                countUnit++;
             }
-            srcNumb = srcNumb / 10;
+            srcNumb /= 10;
         }
-        if (j % 2 == 0) {
-            System.out.print(j + "(четное) количество единиц");
+        if (countUnit % 2 == 0) {
+            System.out.print(countUnit + "(четное) количество единиц");
         } else {
-            System.out.print(j + "(нечетное) количество единиц");
+            System.out.print(countUnit + "(нечетное) количество единиц");
         }
 
         System.out.println("\n\n 6. Отображение фигур в консоли");
 
-        for (int i = 0; i < 50;i++) {
+        for (int i = 0; i < 50; i++) {
             if (i % 10 == 0) {
-                System.out.print("\n*");
+                System.out.println();
             } else {
                 System.out.print("*");
             }
         }
 
         int i = 0;
-        j = 1;
+        int j = 1;
         int k = 5;
-        System.out.print("\n\n");
+        System.out.println("\n");
         while (i < 15) {
             while (j < k) {
                 System.out.print("#");
                 j++;
                 i++;
             }
-            System.out.print("#\n");
+            System.out.println("#");
             j = 1;            
             k--;
             i++;
@@ -121,7 +120,7 @@ public class CyclesTheme {
             do {
                 System.out.print("$");
                 j++;
-            } while ( j <= k);
+            } while (j <= k);
             System.out.print("\n");
 
             
@@ -137,7 +136,7 @@ public class CyclesTheme {
         int tmpCodeChar = 0;
         System.out.printf("%4s", "Dec");
         System.out.printf("%16s", "Char" + "\n"); 
-        for (i = 1; i <= 122 ;i++) {
+        for (i = 1; i <= 122; i++) {
             if ((i <= 47) && (i % 2 != 0)) {
                 System.out.printf("%4s", i);
                 System.out.printf("%16s", (char) i + "\n");
@@ -148,24 +147,49 @@ public class CyclesTheme {
         }
 
         System.out.println("\n 8. Проверка, является ли число палиндромом");
-        srcNumb = 123454331;
-        String srcNumbStr = Integer.toString(srcNumb);
-        j = srcNumbStr.length();
-        if (j % 2 !=0) {
-            boolean isPalindrom = true;
-            for (i = 0;i < j / 2;i++) {
-                if (srcNumbStr.charAt(i) != srcNumbStr.charAt(j - i - 1)) {
-                    isPalindrom = false;
-                }
-            }
-            if (isPalindrom == true) {
-                System.out.println("\nчисло " + srcNumb + " является палиндромом");
-            } else {
-                System.out.println("\nчисло " + srcNumb + " является не палиндромом");
-            }
-        } else {
-            System.out.println("\nчисло " + srcNumb + " является не палиндромом");
+        srcNumb = 72527;
+        int tmpNumbL = srcNumb;
+        int tmpNumbR = srcNumb;
+        int countDig = 0;
+        int exponent = 1; 
+        int leftDig = 0;
+        int rightDig = 0;
+        boolean isPalindrom = true;
+        
+        do {
+            countDig++;
+            tmpNumbR /= 10;
+        } while (tmpNumbR > 0);
+        for (i = 1; i <= countDig / 2; i++) {
+            exponent = 1;
+            int expLev = 1;
+            tmpNumbR = srcNumb;
+            do {
+             exponent *= 10;
+             expLev++;
+         } while (expLev <= i);
+         tmpNumbR = tmpNumbR % exponent;
+         tmpNumbR = tmpNumbR / (exponent / 10);
+         rightDig = tmpNumbR;
+         tmpNumbL = srcNumb;
+         exponent = 1;
+         expLev = countDig - i + 1;
+         k = 1;
+         for (k = 1; k <= expLev; k++) {
+            exponent *= 10;
         }
+        tmpNumbL = tmpNumbL % exponent;
+        tmpNumbL = tmpNumbL / (exponent / 10);
+        leftDig = tmpNumbL;
+        if (leftDig != rightDig) {
+            isPalindrom = false;
+        }
+    }
+    if (isPalindrom == true) {
+        System.out.println("\nчисло " + srcNumb + " является палиндромом");
+    } else {
+       System.out.println("\nчисло " + srcNumb + " является не палиндромом");
+   }
 
         System.out.println("\n 9. Определение, является ли число счастливым");
         srcNumb = 314152;
@@ -195,25 +219,25 @@ public class CyclesTheme {
         }
 
         System.out.println("\n 10. Вывод таблицы умножения Пифагора");
-        for (j = 1;j <= 9;j++) {
+        for (j = 1; j <= 9; j++) {
             if (j == 1) {
-               System.out.print("\n   |");
-           }
-           else {
+             System.out.print("\n   |");
+         }
+         else {
             System.out.printf("%4d", j);
         }
     }
-    for (j = 1;j <= 9;j++) {
+    for (j = 1; j <= 9; j++) {
         if (j == 1) {
             System.out.print("\n");
         } else {
             System.out.print("-----");
         }
     }
-    for (i = 2;i <= 9 ;i++ ) {
+    for (i = 2; i <= 9; i++) {
         System.out.printf("\n%2d", i);
         System.out.print(" |");
-        for (j = 2;j <= 9 ;j++ ) {
+        for (j = 2; j <= 9; j++) {
             if (i == 1) {
                 System.out.printf("%4d", j);
             } 
